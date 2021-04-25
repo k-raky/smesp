d08789b13c1318dc76679f060a15fac72c62763f
raky <kaneraky12@gmail.com>
2021-04-25 16:08:31 +0000

    update

--- implementation/demandeservice.php
+++ implementation/demandeservice.php
@@ -21,44 +21,29 @@ else {
   echo "<script>alert(erreur de requete : $conn->error)</script>";
 }
 
-
-if(isset($_POST['suspendre'])){ 
-
-    global $conn;
-
-    $motif=$_POST['motif'];
-    $refsus=$_POST['refsus'];  
-
-    $query3 = "update taches set statut='suspendue',motifsuspension='$motif' where ref=$refsus";
-    $result3 = mysqli_query($conn,$query3); 
-    if ($result3) {
-      echo "<script>alert('tache suspendue')</script>";
-      unset($_POST);
-      echo "<script>location.href='demandetech.php';</script>";
+function getinfos($conn,$ref){
+    $query2 = "SELECT * FROM taches where ref=$ref";
+    $result2 = mysqli_query($conn,$query2); 
+    if ($result2) {
+        $row2= mysqli_fetch_row($result2);
+        return $row2;
     } 
     else {
       echo "<script>alert(erreur de requete : $conn->error)</script>";
     }
-
 }
 
-if(isset($_POST['reprendre'])){ 
-
-  global $conn;
-  $refrep=$_POST['refrep'];  
-
-  $query4 = "update taches set statut='en cours',motifsuspension=NULL where ref=$refrep;";
-  $result4 = mysqli_query($conn,$query4); 
-  if ($result4) {
-    echo "<script>alert('tache reprise')</script>";
-    unset($_POST);
-    echo "<script>location.href='demandetech.php';</script>";
-  } 
-  else {
-    echo "<script>alert(erreur de requete : $conn->error)</script>";
+function suspendre($ref,$motif,$conn){
+  $query3 = "update taches set statut='suspendue',motifsuspension=$motif where ref=$ref";
+    $result3 = mysqli_query($conn,$query3); 
+    if ($result3) {
+      echo "<script>alert('tache suspendue')</script>";
+    } 
+    else {
+      echo "<script>alert(erreur de requete : $conn->error)</script>";
+    }
   }
 
-}
 
 ?>
 
@@ -115,20 +100,8 @@ if(isset($_POST['reprendre'])){
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
-
 
     <script type="text/javascript">
-
-        $(document).ready(function(){
-          $("#search").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $("#table").filter(function() {
-              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-            });
-          });
-        });
-
 
         function change(delai,ref,statut,button){
 
@@ -147,7 +120,8 @@ if(isset($_POST['reprendre'])){
             document.getElementById(button).style.visibility="visible";
           }
         }
-      
+        
+        var row2;
 
         function statut(statut,ref){
           if (statut=="ok") {
@@ -156,29 +130,8 @@ if(isset($_POST['reprendre'])){
           if (statut=="suspendue") {
             $('#modalsuspendue').modal('show');
           }
-        }
-
-
-        function getref(ref){
-            document.getElementById('refsus').value=ref;
-        }
-
-        function infos(ref,date,nom,contact,fonction,type,cause,depart,statut,delai,motif){
-          document.getElementById("refinfos").innerText=ref;
-          document.getElementById("date").innerText=date;
-          document.getElementById("nom").innerText=nom;
-          document.getElementById("contact").innerText=contact;
-          document.getElementById("fonction").innerText=fonction;
-          document.getElementById("type").innerText=type;
-          document.getElementById("cause").innerText=cause;
-          document.getElementById("depart").innerText=depart;
-          document.getElementById("statut").innerText=statut;
-          document.getElementById("delai").innerText=delai;
-          document.getElementById("motifsuspension").innerText=motif;
-          document.getElementById("refrep").value=ref;
 
         }
-       
         
     </script>
 
@@ -190,12 +143,10 @@ if(isset($_POST['reprendre'])){
    
           <div class="container bg-light d-flex flex-column align-items-center align-self-center">
 
-          <input type="search" name="search" id="search" placeholder="Search..">
-
             <h1 class="titre m-5">LES DEMANDES</h1>
           
           <div>
-            <table class="table" id="table">
+            <table class="table">
                 <thead>
                   <tr>
                     <th scope="col">Ref</th>
@@ -215,27 +166,19 @@ if(isset($_POST['reprendre'])){
                   while($row = mysqli_fetch_assoc($result)){
                     $button="button".$row['ref']."";
                     $ref=$row['ref'];
-                    $date=$row['date'];
-                    $nom=$row['nom'];
-                    $contact=$row['contact'];
-                    $fonction=$row['fonction'];
-                    $type=$row['type'];
-                    $cause=$row['cause'];
-                    $depart=$row['departement'];
-                    $statut=$row['statut'];
                     $delai=$row['delai'];
-                    $motif=$row['motifsuspension'];
-                    echo "<tr id='$ref'><th scope='row'>$ref</th>";
-                    echo "<td>$date</td>";
-                    echo "<td>$nom</td>";
-                    echo "<td>$contact</td>";
-                    echo "<td>$fonction</td>";
-                    echo "<td>$type</td>";
-                    echo "<td>$cause</td>";
-                    echo "<td>$depart</td>";
-                    echo "<td class='statut' onclick='statut(\"$statut\");infos($ref,\"$date\",\"$nom\",$contact,\"$fonction\",\"$type\",\"$cause\",\"$depart\",\"$statut\",$delai,\"$motif\");'>$statut</td>";
-                    echo "<td>$delai</td>";
-                    echo "<td><button class='btn-info btn-lg' id='$button' data-toggle='modal' data-target='#modalupdate' style='visibility:hidden;' onclick='getref($ref);'>mettre a jour</button></td></tr>";
+                    $statut=$row['statut'];
+                    echo "<tr id='$ref'><th scope='row'>".$row['ref']."</th>";
+                    echo "<td>".$row['date']."</td>";
+                    echo "<td>".$row['nom']."</td>";
+                    echo "<td>".$row['contact']."</td>";
+                    echo "<td>".$row['fonction']."</td>";
+                    echo "<td>".$row['type']."</td>";
+                    echo "<td>".$row['cause']."</td>";
+                    echo "<td>".$row['departement']."</td>";
+                    echo "<td class='statut' onclick='statut(\"$statut\",$ref);'>".$row['statut']."</td>";
+                    echo "<td>".$row['delai']."</td>";
+                    echo "<td><button class='btn-info btn-lg' id='$button' data-toggle='modal' data-target='#modalupdate' style='visibility:hidden;'>mettre a jour</button></td></tr>";
                     echo "<script> change($delai,$ref,'$statut','$button');</script>";
                     }
                   ?>
@@ -256,22 +199,13 @@ if(isset($_POST['reprendre'])){
       </div>
     </div>
 
-    <div class="modal fade" id="modalsuspendre">
+    <div class="modal fade" id="modalsuspendre" >
       <div class="modal-dialog modal-dialog-centered modal-md">
         <div class="modal-content">
-          <div class="modal-body p-5 ">
-            <form action="#" class="form d-flex flex-column align-items-center" id="form" method="post">
-              <h3 class="modal-title m-3">Motif de la suspension</h5>
-              <div>
-                  <label for="refsus" class="col-sm-2 col-form-label text-info">Ref</label>
-                  <input type="text" readonly class="m-3" id="refsus" name="refsus">
-              </div>
-              <div>
-                 <label for="motif" class="col-sm-2 col-form-label text-info">Motif</label>
-                <input type="text" class="m-3" id="motif" name="motif">
-              </div>
-              <button type="submit" name="suspendre" style="background-color: #f4900c;color:white;width:30%; margin-top : 10px;" >Valider</button>
-            </form>
+          <div class="modal-body p-5 d-flex flex-column align-items-center">
+          <h3 class="modal-title">Motif de la suspension</h5>
+              <input class="m-3" type="text" name="motif" id="motif">
+            <button type="button" class="btn" style="background-color: #f4900c;color:white" data-dismiss='modal'>Valider</button>
           </div>
         </div>
       </div>
@@ -285,8 +219,73 @@ if(isset($_POST['reprendre'])){
             <p class="font-weight-bold">Informations sur la tache</p>
             <table class="table">
               <thead>
-                <tr class="text-info">
-                    <td>Ref</td>
+                <tr>
+                <td>Ref</td>
+                    <td scope="col">Date</td>
+                    <td scope="col">Nom</td>
+                    <td scope="col">Contact</td>
+                    <td scope="col">Fonction</td>
+                    <td scope="col">Type</td>
+                    <td scope="col">Cause</td>
+                    <td scope="col">Departement</td>
+                    <td scope="col">Statut</td>
+                    <td scope="col">Delai</td>
+                </tr>
+              </thead>
+              <tbody>
+              <?php
+                    echo "<tr class='font-weight-bold'>";
+                    foreach($row2 as $value) {
+                      echo "<td>$value</td>";
+                    }
+                    echo "</tr>";
+                  ?>
+              </tbody>
+            </table>
+            </div>
+            <div>
+            <p class="font-weight-bold">Technicien en charge</p>
+            <table class="table">
+              <thead>
+                <tr>
+                <td scope="col">Id</td>
+                    <td scope="col">Nom</td>
+                    <td scope="col">Contact</td>
+                    <td scope="col">service</td>
+                    <td scope="col">Statut</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td scope="row"></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+            </div>
+            <div>
+            <p class="font-weight-bold">Motif de suspension</p>
+            </div>
+            <button type="button" class="btn" style="background-color: #f4900c;color:white">rependre la tache</button>
+            <button type="button" class="btn" style="background-color: #f4900c;color:white" data-dismiss="modal">ok</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="modalencours" >
+      <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content">
+          <div class="modal-body">
+          <div>
+            <p class="font-weight-bold">Informations sur la tache</p>
+            <table class="table">
+              <thead>
+                <tr>
+                <td scope="col">Ref</td>
                     <td scope="col">Date</td>
                     <td scope="col">Nom</td>
                     <td scope="col">Contact</td>
@@ -300,34 +299,44 @@ if(isset($_POST['reprendre'])){
               </thead>
               <tbody>
                 <tr>
-                  <td id="refinfos"></td>
-                  <td id="date"></td>
-                  <td id="nom"></td>
-                  <td id="contact"></td>
-                  <td id="fonction"></td>
-                  <td id="type"></td>
-                  <td id="cause"></td>
-                  <td id="depart"></td>
-                  <td id="statut"></td>
-                  <td id="delai"></td>
+                  <td scope="row"></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
                 </tr>
               </tbody>
             </table>
             </div>
-            
-            <div class="d-flex">
-            <p class="font-weight-bold">Motif de suspension  : </p>
-            <p id="motifsuspension" class="text-danger" style="margin-left: 15px;"></p>
+            <div>
+            <p class="font-weight-bold">Technicien en charge</p>
+            <table class="table">
+              <thead>
+                <tr>
+                <td scope="col">Id</td>
+                    <td scope="col">Nom</td>
+                    <td scope="col">Contact</td>
+                    <td scope="col">service</td>
+                    <td scope="col">Statut</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td scope="row"></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
             </div>
-
-            <div class="d-flex justify-content-center">
-              <form action="#" method="post">
-                <input type="hidden" name="refrep" id="refrep">
-                <button type="submit" name="reprendre" class="btn" style="background-color: #f4900c;color:white;margin : 15px;">rependre la tache</button>
-              </form>
-            <button type="button" class="btn" name="reprendre" style="background-color: #f4900c;color:white;margin : 15px;" data-dismiss="modal">ok</button>
-            </div>
-
+            <button type="button" class="btn" style="background-color: #f4900c;color:white" data-dismiss="modal">ok</button>
           </div>
         </div>
       </div>
